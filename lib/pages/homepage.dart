74c8e37cff52800b8aacfed44ad079c5a364ff87
Roose48 <roosealisme@gmail.com>
2023-05-02 14:16:37 -0400

    homepage ui

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,3 @@
-import 'package:app/pages/login.dart';
 import 'package:app/pages/places.dart';
 import 'package:app/pages/register.dart';
 import 'package:app/pages/states.dart';
@@ -8,70 +7,65 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        centerTitle: true,
-        title: const Text(
-          'Homepage',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: Center(
+      body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Container(
-              padding: const EdgeInsets.all(15),
-              decoration: const BoxDecoration(
+            const Text(
+              "You want to chill? Discover beautiful places in Haiti with us!",
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
                 color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 5,
-                  ),
-                ],
               ),
-              child: const Text(
-                'Choose one of two options to navigate',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                ),
-              ),
+              textAlign: TextAlign.center,
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
+            GestureDetector(
+              onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => States( title: 'bae', name: 'AllDepartement',)),
+                  MaterialPageRoute(builder: (context) => States( title: 'bae', name: 'AllDepartement')),
                 );
               },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(250, 50),
-                textStyle: TextStyle(fontSize: 20),
-                primary: Colors.white,
-                onPrimary: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  side: const BorderSide(
-                    color: Colors.deepOrange,
-                    width: 2,
+              child: Container(
+                height: 50.0,
+                width: 220.0,
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 197, 67, 28),
+                  borderRadius: BorderRadius.circular(25.0),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 4.0,
+                      spreadRadius: 1.0, 
+                      offset: Offset(
+                        0.0,
+                        4.0,
+                      ),
+                    ),
+                  ]
+                  ),
+                child: const Text(
+                  "Discover",
+                  style: TextStyle(
+                    fontSize: 10.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
               ),
-              child: Text('Thing To Do'),
             ),
+
             const SizedBox(height: 20),
+            
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) =>  Login(title: 'AllDepartement',)),
+                  MaterialPageRoute(builder: (context) =>  Register(title: 'AllDepartement',)),
                 );
               },
               style: ElevatedButton.styleFrom(
